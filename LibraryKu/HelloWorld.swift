937ad9354a143bf5fe6469025ae4014881bac072
fardan-dev <fardan.wardhana@gmail.com>
2023-08-10 16:56:28 +0700

    update code

---
+++ LibraryKu/HelloWorld.swift
@@ -0,0 +1,10 @@
+//
+//  HelloWorld.swift
+//  LibraryKu
+//
+//  Created by Muhamad Fardan Wardhana on 10/08/23.
+//
+
+open class HelloWorld {
+  public static let text = "Hallo Dunia, aku adalah programmer"
+}
---
+++ LibraryKu/LibraryKu.h
@@ -0,0 +1,18 @@
+//
+//  LibraryKu.h
+//  LibraryKu
+//
+//  Created by telkom on 10/08/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LibraryKu.
+FOUNDATION_EXPORT double LibraryKuVersionNumber;
+
+//! Project version string for LibraryKu.
+FOUNDATION_EXPORT const unsigned char LibraryKuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LibraryKu/PublicHeader.h>
+
+
